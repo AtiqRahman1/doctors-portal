7da53ebc5e069568b0b3abd630300c80ffeb9abe
AtiqRahman1 <atiq94.ar@gmail.com>
2022-05-16 20:58:51 +0600

    navbar added

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const menuItems = <>
-        <li><a>Home</a></li>
-        <li><a>Appointment</a></li>
-        <li><a>Review</a></li>
-        <li><a>Contact Us</a></li>
-        <li><a>About</a></li>
-        <li><a>Login</a></li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/appointment'>Appointment</Link></li>
+        <li><Link to='/review'>Review</Link></li>
+        <li><Link to='/contact'>Contact Us</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/login'>Login</Link></li>
     </>
 
     return (
@@ -27,9 +28,6 @@ const Navbar = () => {
                 <ul class="menu menu-horizontal p-0">
                     {menuItems}
                 </ul>
-            </div>
-            <div class="navbar-end">
-                <a class="btn">Get started</a>
             </div>
         </div>
     );
