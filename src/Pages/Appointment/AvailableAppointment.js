3e613e750face2289fd161f8073c2f5f95c517db
AtiqRahman1 <atiq94.ar@gmail.com>
2022-05-20 12:01:11 +0600

    url changes

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -10,7 +10,7 @@ const AvailableAppointment = ({ date }) => {
     const [treatment, setTreatment] = useState(null);
 
     const formattedDate = format(date, 'PP');
-    const { data: services, isLoading, refetch } = useQuery(['available', formattedDate], () => fetch(`http://localhost:5000/available?date=${formattedDate}`).then(res => res.json())
+    const { data: services, isLoading, refetch } = useQuery(['available', formattedDate], () => fetch(`https://powerful-cliffs-16131.herokuapp.com/available?date=${formattedDate}`).then(res => res.json())
     )
 
     if (isLoading) {
